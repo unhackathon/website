67710a0295640a07d32b8c0c7d957caa46b6a475
Timotius Sitorus <timsitorus1@gmail.com>
2015-04-20 12:19:05 -0400

    migrating master to dev

--- gulpfile.js
+++ gulpfile.js
@@ -121,6 +121,7 @@ gulp.task('wiredep', function () {
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({
+      branch: "master"
       force: true
     }));
 });
