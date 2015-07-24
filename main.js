2a8c9985b1375a70248192dbdc9add12cff9d2c1
John Daniels <johnsdaniels@gmail.com>
2015-07-24 09:59:05 -0400

    Added randomized hack background images and Code of Conduct

---
+++ main.js
@@ -0,0 +1,78 @@
+
+var pics = [
+    {
+        img: "pic1.jpg",
+        color: "#38A4FE"
+    },
+    {
+        img: "pic2.jpg",
+        color: "#3D9002"
+    },
+    {
+        img: "pic3.jpg",
+        color: "#34BCA3"
+    },
+    {
+        img: "pic4.jpg",
+        color: "#34BCA3"
+    },
+    {
+        img: "pic6.jpg",
+        color: "#FFC643"
+    },
+    {
+        img: "pic7.jpg",
+        color: "#EC006B"
+    },
+    {
+        img: "pic8.jpg",
+        color: "#DF2025"
+    },
+    {
+        img: "pic9.jpg",
+        color: "#007164"
+    },
+    {
+        img: "pic10.jpg",
+        color: "#2835FD"
+    },
+    {
+        img: "pic11.jpg",
+        color: "#0056B3"
+    },
+    {
+        img: "pic12.jpg",
+        color: "#900025"
+    },
+    {
+        img: "pic13.jpg",
+        color: "#FFC400"
+    },
+    {
+        img: "pic14.jpg",
+        color: "#018AAE"
+    },
+    {
+        img: "pic15.jpg",
+        color: "#860020"
+    },
+    {
+        img: "pic16.jpg",
+        color: "#637440"
+    },
+    {
+        img: "pic17.jpg",
+        color: "#41B4FF"
+    },
+    
+];
+
+function setImage(index) {
+    var item = pics[index];
+    document.getElementById("svg-image").setAttribute("xlink:href", "/img/" + item.img);
+    var applyElement = document.getElementById("apply-span");
+    if (applyElement) {
+        applyElement.style.color = item.color;
+    }
+}
+setImage(Math.floor(Math.random() * pics.length));
